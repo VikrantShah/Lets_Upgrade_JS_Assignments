96765a0700688b00d8727f447c242a204756150c
Vikrant Shah <62543797+VikrantShah@users.noreply.github.com>
2020-07-20 21:16:57 +0530

    Add files via upload

--- Assignment_5/Q5.js
+++ Assignment_5/Q5.js
@@ -8,15 +8,15 @@ if(sales >= 0 && sales <= 5000)
 }
 else if(sales >= 5001 && sales <= 10000)
 {
-    console.log(`Commision earned by you is ${sales * 0.05}`);
+    console.log(`Commision earned by you is ${sales * (0.05 + 0.02)}`);
 }
 else if(sales >= 10001 && sales <= 20000)
 {
-    console.log(`Commision earned by you is ${sales * 0.07}`);
+    console.log(`Commision earned by you is ${sales * (0.07 + 0.05 + 0.02)}`);
 }
 else if(sales > 20000)
 {
-    console.log(`Commision earned by you is ${sales * 0.1}`);
+    console.log(`Commision earned by you is ${sales * (0.1 + 0.07 + 0.05 + 0.02)}`);
 }
 else
 {
